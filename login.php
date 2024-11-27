b7cf41d9bfdd9bb8d8f5612de1d757657392b4b0
9380074813 <153709331+9380074813@users.noreply.github.com>
2024-11-27 15:03:24 +0530

    4

---
+++ login.php
@@ -0,0 +1,51 @@
+
+   <!-- body -->
+   
+    
+      <!-- section blue_bg -->
+      <div class="section blue_bg">
+         <div class="container-fluid">
+            <div class="row">
+               <!-- contact -->
+               <div class="col-md-6 offset-md-3">
+                  <div class="contact">
+                     <div class="row">
+                        <div class="col-md-12">
+                           <div class="titlepage text_align_center">
+                              <h2>User LOGIN</h2>
+                           </div>
+                        </div>
+                        <div class="col-md-12">
+                          
+                            <form id="request" class="main_form" method="POST" action="userlogin">
+                              <div class="row">
+                                
+                                  <div class="col-md-12"  >
+                                    <input style="border-radius:20px;" class="contactus" placeholder="Phone" type="text" name="aname">                          
+                                 </div>                                
+                                <div class="col-md-12">
+                                    <input style="border-radius:20px;"class="contactus" placeholder="Password" type="password" name="password">                          
+                                 </div>                                 
+                                   
+                                     <div class="col-md-12" style="text-align: center;">                                    
+                                     <input style="width: 30%;height:40px; background-color: red;border-radius: 20px; border-color: white;" type="submit" name="login" value="LOGIN">
+                                 </div>  
+                                  
+                                 
+                              </div>
+                           </form>
+                            <div class="col-md-12" style="text-align: " > 
+                            <a href="register" style="padding: 4px 4px;  color:whitesmoke;">NEW USER REGISTRATION</a>
+                            </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+               <!-- contact -->
+            </div>
+            
+         </div>
+          
+      </div>
+    
+
